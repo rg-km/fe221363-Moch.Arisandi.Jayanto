38eae30e6c9f7f4a7c964b85d7bd7466011016dc
MochArisandiJayanto <diajaya0@gmail.com>
2022-04-04 14:36:26 +0700

    Update code diagonal difference

--- frontend/javascript/hackerrank/diagonal-difference/main.js
+++ frontend/javascript/hackerrank/diagonal-difference/main.js
@@ -13,7 +13,15 @@
 
 function diagonalDifference(arr) {
   // Write your code here
+  let leftToRight = 0;
+  let rightToLeft = 0;
+  for (let i = 0; i < arr.length; i++) {
+    leftToRight += arr[i][i];
+    rightToLeft += arr[i][arr.length - 1 - i];
+  }
   // TODO: answer here
+  return Math.abs(leftToRight - rightToLeft);
+
 }
 
 function main() {
