df200ef7c5740e3a016bcdc7b5c276a8cc7845e0
MochArisandiJayanto <diajaya0@gmail.com>
2022-04-05 10:36:26 +0700

    Update code stopwatch

--- frontend/dom/stopwatch-app-assignment/index.js
+++ frontend/dom/stopwatch-app-assignment/index.js
@@ -2,26 +2,18 @@
 Uncomment variable dibawah ini untuk mulai mengerjakan. dilarang mengganti nama variable yang dibuat.
 */
 
-// let stopwatch = mendapatkan value dari stopwatch
-// let startbtn = tombol untuk memulai stapwatch
-// let stopbtn = tombol untuk memberhentikan stopwatch
-// let resetbtn = tombol untuk mereset value dari stopwatch
-
-// TODO: answer here
-
-
-function start() {
-  // TODO: answer here
-}
-
-function stop() {
-  // TODO: answer here
-}
-
-function reset() {
-  // TODO: answer here
-}
-
-function runTime() {
-  // TODO: answer here
-}
+var stopwatch = {
+  startTime: 0,
+  start: function() {
+    stopwatch.startTime = Date.now();
+  },
+  stop: function() {
+    stopwatch.startTime = 0;
+  },
+  reset: function() {
+    stopwatch.startTime = 0;
+  },
+  getTime: function() {
+    return stopwatch.startTime;
+  }
+};
