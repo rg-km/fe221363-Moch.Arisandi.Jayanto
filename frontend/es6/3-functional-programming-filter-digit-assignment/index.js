d81427ce7bf4e0581cd27e50f9377c808f5cf94a
MochArisandiJayanto <diajaya0@gmail.com>
2022-04-25 08:51:51 +0700

    Update filter digit

--- frontend/es6/3-functional-programming-filter-digit-assignment/index.js
+++ frontend/es6/3-functional-programming-filter-digit-assignment/index.js
@@ -11,6 +11,11 @@
 
 const filterDigit = (array, digit) => {
   // TODO: answer here
+  return array.filter(item => item.toString().length === digit);
 };
+
+const a = [88, 44, 3, 8481, 444];
+const b = filterDigit(a, 2);
+console.log(b); // [88, 44]
 
 module.exports = filterDigit
