070eb56e009dc3125bf211c61d1a7da59698d195
MochArisandiJayanto <diajaya0@gmail.com>
2022-04-25 09:25:48 +0700

    Update spread suntax

--- frontend/es6/4-spread-syntax-assignment/index.js
+++ frontend/es6/4-spread-syntax-assignment/index.js
@@ -5,8 +5,13 @@
  * Output: {a: 1, b: 2, c: 3, d: 4}
  */
 
-const mergeTwoObjects = (firstObject, secondObject) => {
+ const mergeTwoObjects = (firstObject, secondObject) => {
   // TODO: answer here
+  const mergedObject = { ...firstObject, ...secondObject };
+  return mergedObject;
 };
 
+let a = { a: 1, b: 2 };
+let b = { c: 3, d: 4 };
+console.log(mergeTwoObjects(a, b));
 module.exports = mergeTwoObjects
