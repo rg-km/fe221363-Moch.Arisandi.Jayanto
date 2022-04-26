149484cb42284422a1fa29787f71d226e40a3829
MochArisandiJayanto <diajaya0@gmail.com>
2022-04-26 21:11:27 +0700

    Update music playlist

--- frontend/data-structure/assignment/music-playlist/music-player.js
+++ frontend/data-structure/assignment/music-playlist/music-player.js
@@ -8,9 +8,18 @@ module.exports = class MusicPlayer {
 
     addSong(song) {
         // TODO: answer here
+        this.playlist.songs.push(song);
     }
 
     play() {
         // TODO: answer here
+        if (this.playlist.songs.length === 0) {
+			return "";
+		}
+		let keluar = this.playlist.songs.shift();
+		if (this.playlist.isRepeatable) {
+			this.playlist.songs.push(keluar);
+		}
+		return `Now Playing ${keluar.singer} - ${keluar.title}`;
     }
-}
+};
