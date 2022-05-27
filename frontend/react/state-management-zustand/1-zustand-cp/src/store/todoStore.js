89aa88562c4936a78a128b6a3ae1198e6e1fc81b
MochArisandiJayanto <diajaya0@gmail.com>
2022-05-27 10:57:15 +0700

    Update code zustand cp

--- frontend/react/state-management-zustand/1-zustand-cp/src/store/todoStore.js
+++ frontend/react/state-management-zustand/1-zustand-cp/src/store/todoStore.js
@@ -1,6 +1,27 @@
 import create from 'zustand';
+// - `addTodo({id, text, isDone})` : function for adding new todo to array
+// - `removeTodo(id)` : function for removing todo (using `id` for parameter)
+// - `toggleTodo(id)`: function for toggling todo is done or not (using `id` for parameter)
 
 const useTodoStore =
 // TODO: answer here
 
+create((set) => ({
+    todos: [],
+    addTodo: (todo) => {
+        set((state) => ({
+            todos: [...state.todos, todo],
+        }));
+    },
+    removeTodo: (id) => {
+        set((state) => ({
+            todos: state.todos.filter((todo) => todo.id !== id),
+        }));
+    },
+    toggleTodo: (id) => {
+        set((state) => ({
+            todos: state.todos.map((todo) => (todo.id === id ? { ...todo, isDone: !todo.isDone } : todo)),
+        }));
+    },
+}));
 export default useTodoStore;
